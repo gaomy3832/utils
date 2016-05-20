305fe92ab28b99b3d2d6b51e2dd68a8f2dc07831
Mingyu Gao <mgao12@stanford.edu>
2016-05-20 16:22:39 -0700

    Lint string_utils.h. Change reference to pointer.

--- include/utils/string_utils.h
+++ include/utils/string_utils.h
@@ -6,19 +6,21 @@
 #include <string>
 #include <vector>
 
-// Tokenize.
-// From http://oopweb.com/CPP/Documents/CPPHOWTO/Volume/C++Programming-HOWTO-7.html
-inline void Tokenize(const std::string& str, std::vector<std::string>& tokens,
+/**
+ * Tokenize.
+ */
+// http://oopweb.com/CPP/Documents/CPPHOWTO/Volume/C++Programming-HOWTO-7.html
+inline void tokenize(const std::string& str, std::vector<std::string>* tokens,
         const std::string& delimiters = " ") {
-
+    // Skip nullptr.
+    if (tokens == nullptr) return;
     // Skip delimiters at beginning.
     std::string::size_type lastPos = str.find_first_not_of(delimiters, 0);
     // Find first "non-delimiter".
     std::string::size_type pos     = str.find_first_of(delimiters, lastPos);
-
     while (std::string::npos != pos || std::string::npos != lastPos) {
         // Found a token, add it to the vector.
-        tokens.push_back(str.substr(lastPos, pos - lastPos));
+        tokens->push_back(str.substr(lastPos, pos - lastPos));
         // Skip delimiters.  Note the "not_of"
         lastPos = str.find_first_not_of(delimiters, pos);
         // Find next "non-delimiter"
@@ -26,5 +28,5 @@ inline void Tokenize(const std::string& str, std::vector<std::string>& tokens,
     }
 }
 
-#endif // UTILS_STRING_UTILS_H_
+#endif  // UTILS_STRING_UTILS_H_
 
