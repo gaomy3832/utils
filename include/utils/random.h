cc261d2c4e73567c7b283545a89d64e822ba82ab
Mingyu Gao <mgao12@stanford.edu>
2016-05-20 17:09:47 -0700

    Change the implementation of Random.

--- include/utils/random.h
+++ include/utils/random.h
@@ -3,25 +3,57 @@
 /**
  * Random number generator.
  */
-#include <random>   // for std::mt19937, std::uniform_real_distribution
+#include <limits>
+#include <random>
+#include "utils/log.h"
 
-template<typename Type>
-class UniformRand {
-    public:
-        UniformRand(Type min, Type max, unsigned seed) : gen_(seed), dst_(min, max) {}
+class Random {
+public:
+    typedef IntType uint64_t;
+    typedef RealType double;
 
-        Type get() { return dst_(gen_); }
+public:
+    explicit Random(uint64_t seed)
+            : prng(seed) {
+        // Nothing else to do.
+    }
 
-        template<typename Iter>
-        void assign(Iter begin, Iter end) {
-            for (Iter it = begin; it != end; it++) {
-                *it = get();
-            }
-        }
+    Random()
+            : Random(std::random_device()()) {
+        // Nothing else to do.
+    }
 
-    protected:
-        std::mt19937 gen_;
-        std::uniform_real_distribution<Type> dst_;
+    IntType getInteger() {
+        return intDist(prng);
+    }
+
+    IntType getInteger(IntType min, IntType max) {
+        assert(max >= min);
+        if (max == min) return min;
+        auto r = getInteger();
+        auto span = max - min;
+        if (span == std::numerical_limits<IntType>::max())
+            return r;
+        else
+            return min + r % (span + 1);
+    }
+
+    RealType getReal() {
+        return realDist(prng);
+    }
+
+    RealType getReal(RealType min, RealType max) {
+        assert(max >= min);
+        auto r = getReal();
+        auto span = max - min;
+        return min + r * span;
+    }
+
+private:
+    std::mt19937_64 prng;
+
+    std::uniform_int_distribution<uint64_t> intDist;
+    std::uniform_real_distribution<double> realDist;
 };
 
 #endif  // UTILS_RANDOM_H_
