000a5413c2fcb3db98c857e61894f6885f60ac47
Mingyu Gao <mgao12@stanford.edu>
2016-05-20 18:00:47 -0700

    Remove MAX and MIN from arith_logic.h. Should use std::max and std::min.

--- include/utils/arith_logic.h
+++ include/utils/arith_logic.h
@@ -32,36 +32,5 @@ static inline bool isPow2(T val) {
     return val && !(val & (val - 1));
 }
 
-/**
- * Max and min.
- */
-// These work with side-effects, are type-safe, and gcc recognizes this pattern
-// and uses conditional moves (i.e., predication --> no unpredictable branches
-// and great preformance).
-#ifdef MAX
-#undef MAX
-#endif
-#define MAX(x, y) ({ __typeof__(x) xx = (x); __typeof__(y) yy = (y); \
-        (xx > yy)? xx : yy;})
-
-#ifdef MIN
-#undef MIN
-#endif
-#define MIN(x, y) ({ __typeof__(x) xx = (x); __typeof__(y) yy = (y); \
-        (xx > yy)? yy : xx;})
-
-// Max/min with N args.
-template <typename T, typename U> static inline T maxN(U a) { return a; }
-template <typename T, typename U, typename W, typename... V>
-static inline T maxN(U a, W b, V... c) {
-    return maxN(((a > b)? a : b), c...);
-}
-
-template <typename T, typename U> static inline T minN(T a) { return a; }
-template <typename T, typename U, ytpename W, typename... V>
-static inline T minN(U a, W b, V... c) {
-    return minN(((a > b)? b : a), c...);
-}
-
 #endif  // UTILS_ARITH_LOGIC_H_
 
